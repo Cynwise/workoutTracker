2711238f398b1a90783f56f3b0167bd55a327cc5
William Farnsworth <williamcoreyf@gmail.com>
2021-06-18 18:44:53 -0400

    implemented api & htmlRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' },
+            },
+        },
+    ])
+        .then((data) => {
+            res.json(data);
+        }).catch(err => {
+            res.status(400).json(err)
+        });
+
+});
+
+router.post('/api/workouts', ({ body }, res) => {
+    Workout.create(body)
+        .then(data => { 
+            res.json(data)
+        }).catch(err => {
+            res.status(400).json(err)
+        })
+
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true, runValidators: true })
+        .then(data => {
+            res.json(data)
+        }).catch(err => {
+            res.status(400).json(err)
+        })
+});
+
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' },
+            }
+        }
+    ])
+        .then(data => {
+            res.json(data)
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
